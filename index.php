7b6382ef33d5bd74ebed90a32de1b5db72be9869
yoshiyuki-tencent <634554815@qq.com>
2016-08-01 17:16:35 +0800

    UPDATE

--- index.php
+++ index.php
@@ -1,121 +1,131 @@
-<!doctype>
+<!DOCTYPE HTML>
 <html>
 <head>
-	<title>安全库存维护</title>
-	<meta charset="UTF-8">
-	<style type="text/css">
-	table, th, td { border: 1px solid black;text-align: center;}
-	table{border-collapse:collapse;margin: 10px}
-	td{width: 100px;}
-	button{border: 1px solid black;border-radius:5px;}
-	a{padding: 2px;background-color: #F0F0F0 ;border: 1px solid black;border-radius:5px;}
-	#container,#searchbox,#layout{border: 1px solid black;}
-	.header{background-color: #33CCFF}
-	.field{background-color: #AAC6FF}
-	#layout{display: none;margin: 30px}
-	body .demo-class .layui-layer-title{background-color: #33CCFF ;border:1px solid black;}
-	body .demo-class .layui-layer-btn .layui-layer-btn1{background:#999;}
-	</style>
-	<script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
-	<script src="./layer.js"></script>
-	<script src="./grid.js"></script>
-	<script type="text/javascript">
-	var layer,option,param,page,pagesize;
-	$(document)
-	.ready(
-		function () {
-			option = {"key":key, "data":data, "url":false};
-			grid({"key":key, "url":"./json.php", param:{MaterialsName:"123"}});
-			$("body").on("click","button",function(){
-				//使用on(event,selector,function)对动态元素生效
-				switch(this.id){
-					default:
-					$("#nameinput").val($("#MaterialsName"+this.id.replace("Modifiable","")).text());
-					$("#numberinput").val($("#MaterialsNumber"+this.id.replace("Modifiable","")).text());
-					$("#upperinput").val($("#SafeUpperLimit"+this.id.replace("Modifiable","")).text());
-					$("#lowerinput").val($("#SafeLowerLimit"+this.id.replace("Modifiable","")).text());
-					layout();
-					break;
-				}
-			});
+<title>发票校验管理</title>
+<meta charset="UTF-8">
+<style type="text/css">
+table, th, td { border: 1px solid black;text-align: center;}
+table{border-collapse:collapse;margin: 10px}
+td{width: 100px;}
+button{border-radius:5px;}
+#container,#searchbox,#layout{border: 1px solid black;}
+.header{background-color: #33CCFF}
+.field{background-color: #AAC6FF}
+#layout{display: none;margin: 30px}
+body .demo-class .layui-layer-title{background-color: #33CCFF ;border:1px solid black;}
+body .demo-class .layui-layer-btn .layui-layer-btn1{background:#999;}
+a{color:#000;text-decoration: none;}
+</style>
+<script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://kirisamenana.com/frontend/sliontek/layer.js"></script>
+<script src="http://kirisamenana.com/frontend/sliontek/grid.js"></script>
+<script type="text/javascript">
+var layer,option,varjson,page,pagesize=5,grid;
+$(document).ready(
+function () {
+	option = {
+			  "field":key,
+			  "key":key, //data:data,
+			  "param":{"page":1,"pagesize":5},
+			  "url":"./json.php",
+			  "onClick": tdclick,
+			  "replace":replace
+			 }
+	grid = $("#container").Grid(option);
+
+	$("body").on("click","button",function(){
+		//使用$('body').on()对动态元素生效
+		switch(this.id){
+			case "search":
+			grid.setParam($("#query").serializeJson());
+			grid.Grid(grid.opts);
+			$('#query')[0].reset();
+			break;
+
+			case "reset":
+			$('#query')[0].reset();
+			break;
+
+			default:
+			$("#nameinput").val($("#MaterialsName"+this.id.replace("editor","")).text());
+			$("#numberinput").val($("#MaterialsNumber"+this.id.replace("editor","")).text());
+			$("#upperinput").val($("#SafeUpperLimit"+this.id.replace("editor","")).text());
+			$("#lowerinput").val($("#SafeLowerLimit"+this.id.replace("editor","")).text());
+			//layout();
+			break;
 		}
-		);
-	function layout(){
-		layer.open({
-		skin: 'demo-class',
-        type: 1,
-        area: ['500px', '300px'],
-        title:"安全库存定义",
-        btn:["确定","取消"],
-        closeBtn :false,
-        yes:yes,
-        shadeClose: true, //点击遮罩关闭
-        content: $('#layout')
-    	});
-	}
-	function yes (){
-		$.ajax({
-			url  : "SafeMaterialsStorage.php",
-			type : 'post',
-			data : $("#update").serialize(),
-			success:layer.closeAll()
-		});
-		//layer.closeAll();
-	}
-	data = [{"MaterialsNumber":1,
-				"MaterialsName":"name",
-				"MaterialsType":"type",
-				"SafeUpperLimit":1000,
-				"SafeLowerLimit":200,
-				"CurrentStorage":888,
-				"Modifiable":true},
-				{"MaterialsNumber":2,
-				"MaterialsName":"name",
-				"MaterialsType":"type",
-				"SafeUpperLimit":1000,
-				"SafeLowerLimit":200,
-				"CurrentStorage":888,
-				"Modifiable":true},
-				{"MaterialsNumber":3,
-				"MaterialsName":"name",
-				"MaterialsType":"type",
-				"SafeUpperLimit":1000,
-				"SafeLowerLimit":200,
-				"CurrentStorage":888,
-				"Modifiable":true},
-				{"MaterialsNumber":4,
-				"MaterialsName":"name",
-				"MaterialsType":"type",
-				"SafeUpperLimit":1000,
-				"SafeLowerLimit":200,
-				"CurrentStorage":888,
-				"Modifiable":true},
-				{"MaterialsNumber":5,
-				"MaterialsName":"name",
-				"MaterialsType":"type",
-				"SafeUpperLimit":1000,
-				"SafeLowerLimit":200,
-				"CurrentStorage":888,
-				"Modifiable":true}
-				].reverse();
-	key = {	"MaterialsNumber":"物料编号",
-			"MaterialsName":"物料名称",
+	});
+}
+);
+function layout(){
+	layer.open({
+			skin: 'demo-class',
+		    type: 1,
+		    area: ['500px', '300px'],
+		    title:"安全库存定义",
+		    btn:["确定","取消"],
+		    closeBtn :false,
+		    yes:yes,
+		    shadeClose: true, //点击遮罩关闭
+		    content: $('#layout')
+	});
+}
+function yes (){
+	$.ajax({
+		url  : "SafeMaterialsStorage.php",
+		type : 'post',
+		data : $("#update").serialize(),
+		success:layer.closeAll()
+	});
+	//layer.closeAll();
+}
+var replace = function(value){
+    	switch(value){
+    		case "到货确认":
+    		return "<button>到货确认</button>";
+    		break;
+    		case "发票查验":
+    		return "<button>发票查验</button>";
+    		break;
+    		default:
+    		return value;
+    		break;
+    	}
+    };
+function tdclick(){
+	alert(this);
+}
+data = [   {"OrderNumber":1,
+			"EngineerNumber":"1号工程",
+			"MaterialsType":"工程类",
+			"BuyDate":"2月30日",
+			"DeleveryConfirm":"还没确定",
+			"InvoiceCheck":"还没确定",
+			"Modifiable":true}].reverse();
+key = {
+			"OrderNumber":"采购订单编号",
+			"EngineerNumber":"工程编号",
 			"MaterialsType":"物料类型",
-			"SafeUpperLimit":"安全上限",
-			"SafeLowerLimit":"安全下限",
-			"CurrentStorage":"当前库存",
-			"Modifiable":"操作"
+			"BuyDate":"采购日期",
+			"DeleveryConfirm":"确认到货",
+			"InvoiceCheck":"发票校验"	
 			}
-	</script>
+</script>
 </head>
 <body>
 	<div id="searchbox">
 		<div class="header">查询</div>
 		<form name="query" id="query" method="post" action="">
-			<label for="MaterialsName">物料名称</label>
-			<input type="text" id="MaterialsName" name="MaterialsName">
-			<label for="MaterialsId">物料编号</label>
-			<input type="text" id="MaterialsId" name="MaterialsId">
+			<label for="OrderNumber">订单编号</label>
+			<input type="text" id="OrderNumber" name="OrderNumber">
+			<label for="EngineerNumber">工程编号</label>
+			<input type="text" id="EngineerNumber" name="EngineerNumber"><br/>
+			<label for="FormDate">起始日期</label>
+			<input type="date" id="FormDate" name="FormDate">
+			<label for="ToDate">截止日期</label>
+			<input type="date" id="ToDate" name="ToDate">
+			<input type="hidden" name="page" value="1"/>
+			<input type="hidden" name="pagesize" value="5"/>
 		</form>
 	</div>
 	<button id="search">查询</button>
@@ -136,9 +146,18 @@
 		</tbody>
 	</table>
 	<div id="pager" style="text-align: center;">
+		<button id="former">上一页</button>
+		<span>
+			当前页码
+			<span id="page">1</span>
+			/
+			<span id="total"></span>
+			页
+		</span>
+		<button id="latter">下一页</button>
 	</div>
 </div>
-<div id="layout">
+	<div id="layout">
 	<div class="field">明细</div>
 	<form name="update" id="update" action="" method="post">
 	<label>
--- json.php
+++ json.php
@@ -1,57 +1,59 @@
 ﻿<?php
-if (isset($_POST['MaterialsName'])&&!($_POST['MaterialsName']=="")) {
+$result = array();
+$result['page'] = 4;
+if ($_POST['page']>$result['page']) {
+	$_POST['page']=$result['page'];
+}
+if (isset($_POST['OrderNumber'])&&!($_POST['OrderNumber']=="")) {
 	//var_dump(file_get_contents('php://input'));
+	$result = array();
 	$json = array();
-	$json[0] = array('MaterialsNumber' => $_POST['MaterialsName'],
-					 "MaterialsName"=>"name",
-					 "MaterialsType"=>"type",
-					 "SafeUpperLimit"=>1000,
-					 "SafeLowerLimit"=>200,
-					 "CurrentStorage"=>888,
-					 "Modifiable"=>true
+	$json[0] = array("OrderNumber"=>$_POST['OrderNumber'],
+					"EngineerNumber"=>"1号工程",
+					"MaterialsType"=>"工程类",
+					"BuyDate"=>"2月30日",
+					"DeleveryConfirm"=>"到货确认",
+					"InvoiceCheck"=>"发票查验",
+					"Modifiable"=>true
 					 );
-	echo json_encode($json);
+	$result['page'] = 1;
+	$result['data'] = $json;
+	echo json_encode($result);
 	exit();
 }
+
 $json = array();
-$json[0] = array('MaterialsNumber' => 123,
-				 "MaterialsName"=>"name",
-				 "MaterialsType"=>"type",
-				 "SafeUpperLimit"=>1000,
-				 "SafeLowerLimit"=>200,
-				 "CurrentStorage"=>888,
-				 "Modifiable"=>true
-				 );
-$json[1] = array('MaterialsNumber' => 124,
-				 "MaterialsName"=>"name",
-				 "MaterialsType"=>"type",
-				 "SafeUpperLimit"=>1000,
-				 "SafeLowerLimit"=>200,
-				 "CurrentStorage"=>888,
-				 "Modifiable"=>true
-				 );
-$json[2] = array('MaterialsNumber' => 124,
-				 "MaterialsName"=>"name",
-				 "MaterialsType"=>"type",
-				 "SafeUpperLimit"=>1000,
-				 "SafeLowerLimit"=>200,
-				 "CurrentStorage"=>888,
-				 "Modifiable"=>true
-				 );
-$json[3] = array('MaterialsNumber' => 124,
-				 "MaterialsName"=>"name",
-				 "MaterialsType"=>"type",
-				 "SafeUpperLimit"=>1000,
-				 "SafeLowerLimit"=>200,
-				 "CurrentStorage"=>888,
-				 "Modifiable"=>true
-				 );
-$json[4] = array('MaterialsNumber' => 124,
-				 "MaterialsName"=>"name",
-				 "MaterialsType"=>"type",
-				 "SafeUpperLimit"=>1000,
-				 "SafeLowerLimit"=>200,
-				 "CurrentStorage"=>888,
-				 "Modifiable"=>true
-				 );
-echo json_encode($json);
+if (isset($_POST['page'])) {
+	$page = $_POST['page'];
+	$pagesize = 5 ;
+	if (isset($_POST['pagesize'])&&(!$_POST['pagesize']=="")){$pagesize = $_POST['pagesize'];}
+	$from = $pagesize * ($page - 1);
+	$to = $pagesize * $page - 1;
+		if (isset($_POST['OrderNumber'])&&!($_POST['OrderNumber']=="")) {
+		//var_dump(file_get_contents('php://input'));
+		$json = array();
+		$json[0] = array("OrderNumber"=>$_POST['OrderNumber'],
+						"EngineerNumber"=>"1号工程",
+						"MaterialsType"=>"工程类",
+						"BuyDate"=>"2月30日",
+						"DeleveryConfirm"=>"到货确认",
+						"InvoiceCheck"=>"发票查验",
+						"Modifiable"=>true
+						 );
+		}
+	for ($i=$from; $i <= $to; $i++) {
+		$json[$i] = array("OrderNumber"=>$i,
+				"EngineerNumber"=>"1号工程",
+				"MaterialsType"=>"工程类",
+				"BuyDate"=>"2月30日",
+				"DeleveryConfirm"=>"到货确认",
+				"InvoiceCheck"=>"发票查验",
+				"Modifiable"=>true
+					 );
+	}
+}
+
+$result['data'] = $json;
+
+//echo json_encode($json);
+echo json_encode($result);
