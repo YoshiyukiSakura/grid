cabc275980692398a7d5a0b8879e5a84fd0ac0f6
yoshiyuki-tencent <634554815@qq.com>
2016-08-11 13:11:42 +0800

    usual update

--- README.md
+++ README.md
@@ -1,44 +1,74 @@
 # HOW TO USE MY GRID
+##上手
 ##START
+引入jQuery库,grid.js文件
 use the jQuery library and the grid.js file
 ```
 <script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://kirisamenana.com/frontend/sliontek/layer.js"></script>
 <script src="http://kirisamenana.com/frontend/sliontek/grid.js"></script>
 ```
+使用jQuery选择器来调用Grid()方法
 initial grid like this
 ```
 <script>
 grid = $("#container").Grid();
 </script>
 ```
+##参数说明
 ##ARGUMENTS
+这个Grid()方法接收一个对象参数,通常至少需要有【字段】、【数据】等属性;
 $().Grid() recepts a objects containing all the options;
 Basically,you need at least serveral options to initial the grid correctly;
 ```
-var options = {"field":key,"url":"./whatever.php","param":{"page":1}}
+var options = {"field":field,"url":"./whatever.php","param":{"page":1}}
 ```
-###key
-you must define a 'key' to popular the grid,it should be a object like this
+###字段属性
+###field
+要使用Grid,必须定义一个field字段属性,用于查找数据、填充表头等,field属性本身也是一个对象;
+you must define a 'field' to popular the grid,it should be a object like this
 ```
-var key = {fieldinjson:"text in the table"}
+option.field = {field:"field in json",text:"text show in table",replace:function(){} }
 ```
-grid() will use fieldinjson as key to get value from 'data',and use fieldintable to popular table 
+field属性用于在json查找数据,text属性用于填充表头;
+###数据
 ###data
+你可以直接传入一个对象作为数据来填充表格,在option的data属性中传入这个对象即可;
 if you want to use a javascript variable popularing the grid , pass it like this:
 ```
-grid({key:key,data:data})
+grid({key:key,dataArr:data})
 ```
+传入的数据必须是一个对象数组,由于本插件是为使用URL的情况编写的,直接传入数据不被推荐,翻页部分也没有适配
 data must be array of objects like [{key:variable}]
 ###url
+###使用返回json数据的URL接口
+通常配合后端使用的情况都是用一个接口来提供json数据,并且接口可以接收参数来定制输出的内容,你可以在param属性里传入一个参数对象(来自表单或者其他),返回的json数据要求有一定的格式,带有page,size等额外参数;
 if want to use json posted by server,make sure the response being a object array just like 'data'
 you may also use {key:key,url:url,param:param} to post something to server before getting response
+返回的json实例
+a response json example
+```
+{"page":100,"data":[{"OrderNumber":1,"EngineerNumber":"1\u53f7\u5de5\u7a0b"}]}
+```
+##翻页功能
 ##PAGE BUTTON
+翻页功能目前对传入URL的情况适配比较好,直接传入数据的情况待补全;
+目前建议在传入数据的情况下设置option.pager = false 来禁用翻页,针对单页少量数据的情况
 for now,the pager works for satuations using "url" rather than "data";
 if you have passed in a url,it will create a pager automatically;
+##搜索框
 ##SEARCH BOX
-just use a form,and pass it as param
-in my case,the form contains page,pagesize,search conditiongrid({"key":key, "url":"./json.php", "param":$("#query").serialize()});
+目前Grid没有带内置的搜索功能,但是你可以借助外部的表单和setParam()方法来传入参数,实现搜索的功能,具体的搜索由后端完成,Grid只负责传入condition条件,辅助后端构成一个含有like的SQL语句;
+this grid doesn't contain a search function,instead,you may post some params to the server and let server side to deal with it.
+The param data can come from a form or somewhere else,there are two ways to excute a search.
+####如何传入参数
+####pass the param to $().Grid();
 ```
-grid({"key":key, "url":"./json.php", "param":$("#query").serialize()});
+$("#container").Grid(option={param:your data objects});
 ```
+####use the .setParam function,which requires grid objects;
+```
+grid.setParam(your data objects);
+grid.Grid(grid.opts);
+```
+serializeJson() is a function I copied from Internet and God damn know what site is it from;It helps you converting a input form to a objects and doesn't work on select stuations;
--- grid.js
+++ grid.js
@@ -2,12 +2,13 @@
 'use strict';
 (function ($) {
 $.fn.Grid = function (option) {
+	var self = this;
 	//Merge the contents of two or more objects together into the first object.
-	this.opts = $.extend({}, $.fn.Grid.defaults, option);
-	this.setParam = function(param){
-		this.opts.param = $.extend(true, {}, this.opts.param, param);
+	self.opts = $.extend({}, $.fn.Grid.defaults, option);
+	self.setParam = function(param){
+		self.opts.param = $.extend(true, {}, self.opts.param, param);
 	}
-	this.pager = function (current, total) {
+	self.constructpager = function (current, total) {
 	    var first = "<a href=\"#\" class=\"pager\" id=\"first\">首页　</a>",
 	    	prev = current*1-1,
 	    	next = current*1+1,
@@ -15,134 +16,134 @@ $.fn.Grid = function (option) {
 	    	last = "<a href=\"#\" class=\"pager\" id=\"last\">　尾页</a>";
     	if (current=="1") {prev = 1};
     	if (current==total) {next = total};
-	    $("#pager").empty();
-	    $(first).appendTo($("#pager"));
-	    $("<a href=\"#\" class=\"pager\" id=\""+prev+"\">上一页　</a>").appendTo($("#pager"));//上一页按钮
-	    for (var minus=5; minus>0; minus--) //当前开始的前五页pager
-    	{if(current*1-minus>0)
-			{$("<a href=\"#\" class=\"pager\" id=\""+(current*1-minus)+"\">"+(current*1-minus)+"　</a>").appendTo($("#pager"));}
+	    $("#"+self.pagerId, self).empty();
+	    $(first).appendTo($("#"+self.pagerId, self));
+	    $("<a href=\"#\" class=\"pager\" id=\""+prev+"\">上一页　</a>").appendTo($("#"+self.pagerId, self));//上一页按钮
+	    if(self.opts.former5pages==true){
+		    for (var minus=5; minus>0; minus--) //当前开始的前五页pager
+	    	{if(current*1-minus>0)
+				{$("<a href=\"#\" class=\"pager\" id=\""+(current*1-minus)+"\">"+(current*1-minus)+"　</a>").appendTo($("#"+self.pagerId, self));}
+			}
 		}
-	    $(currentPage).appendTo($("#pager"));
+	    $(currentPage).appendTo($("#"+self.pagerId, self));//当前页不可点击
 	    for (var plus=1;  plus<6;  plus++) //当前开始的后五页pager
     	{	if(current*1+plus<=total)
-    		{$("<a href=\"#\" class=\"pager\" id=\""+(current*1+plus)+"\">"+(current*1+plus)+"　</a>").appendTo($("#pager"));}
+    		{$("<a href=\"#\" class=\"pager\" id=\""+(current*1+plus)+"\">"+(current*1+plus)+"　</a>").appendTo($("#"+self.pagerId, self));}
 		}
-		$("<a href=\"#\" class=\"pager\" id=\""+next+"\">　下一页</a>").appendTo($("#pager"));//下一页
-		$(last).appendTo($("#pager"));
-		return this;
-		function makeNumberPagers(current){
-			var plusOne = "<a href=\"#\" class=\"pager\" id=\""+(current*1+1)+"\">"+(current*1+1)+"</a>";
-		}
+		$("<a href=\"#\" class=\"pager\" id=\""+next+"\">　下一页</a>").appendTo($("#"+self.pagerId, self));//下一页
+		$(last).appendTo($("#"+self.pagerId, self));
+		return self;
 	}
-	this.changePage = function(target){
+	self.changePage = function(target){
 		switch(target){
 			case "first":
-			this.opts.param.page = 1;
+			self.opts.param.page = 1;
 			break;
 			case "last":
-			this.opts.param.page = this.opts.totalpage;
+			self.opts.param.page = self.opts.totalpage;
 			break;
 			default:
-			this.opts.param.page = target;
+			self.opts.param.page = target;
 			break;
 		}
-		this.Grid(this.opts);
-		return this;
+		return self.Grid(self.opts);
 	}
-	this.pagedown = function(){
-		if (this.opts.param.page==this.opts.totalpage) {$("#latter").hide();return this;};
-		this.opts.param.page++;
-		this.Grid(this.opts);//俺もよくわからん,window.optionはこれについで変化する
-		return this;
-	};
-	this.pageup   = function(){
-		if (this.opts.param.page=="1") {$("#former").hide();return this;};
-		this.opts.param.page--;
-		this.Grid(this.opts);
-		return this;
-	};
-	this.loadData = function (url){
-		var thisGrid = this;//catch current grid object for in $.ajax() 'this' will be other
+	self.loadData = function (url){
 		var xhr=$.ajax(url,
 					{
 						"async" : false,
 						"method" : "post",
-					    "data" : thisGrid.opts.param,
+					    "data" : self.opts.param,
 					    "dataType" : "json",
 					    "success" : function(data){
-					    	thisGrid.opts.dataArr = data.data;
-					    	thisGrid.opts.totalpage = data.page;
+					    	self.opts.dataArr = data.data;
+					    	self.opts.totalpage = data.page;
 					    }
 					});
 		return xhr;
 	}
-	if (this.opts.data){this.opts.dataArr = option.data}
-	if (this.opts.url) {this.loadData(option.url);}
-	if (typeof(this.opts.replace)=="function") {replace=option.replace};
-	this.popularfield = function(){
+	if (self.opts.json){self.opts.dataArr = option.json.dataArr}//传入data时暂无翻页功能
+	if (self.opts.url) {self.loadData(option.url);}
+	self.popularfield = function(){
 		//遍历字段，为表格填充中文字段内容，一般在首行
-		for(var offset in this.opts.field)
-		{$("#field").append("<td>"+this.opts.field[offset]+"</td>");}
-		return this.opts.field;
-	};
-	this.popularcontent = function(){
-			//you may do this in a OOP way but I prefer my way
+		for(var offset in self.opts.field)
+		{
+			$("#field",self).append("<td calss=\"field\" id=\""+offset+"\">"+self.opts.field[offset].text+"</td>");
+
+		}
+		self.pagerId = Math.random()*100000000000000000;
+		$("#field",self).after("<tr><td colspan="+Object.keys(self.opts.field).length+" id=\""+self.pagerId+"\" style=\"text-align: center;\"></td><tr>");
+		return self.opts.field;
+	}
+	self.popularcontent = function(){
 			//填充数据行
-		for(var offset in this.opts.dataArr)
-			{
-				$("#field").after("<tr class=\"grid-content\" id=\"line"+offset+"\"></tr>");
-				var thisline = $("#line"+offset);
-					for (var key in this.opts.field) {
-						if (key==this.opts.id)//默认使用传入的一个字段的值作为其他字段的ID(未实现)
-							{var tempID = this.opts.dataArr[offset][key]};
+		var offset, lineid,thisline,Data,Field;
+		for( offset in self.opts.dataArr)
+			{	
+				Data = self.opts.dataArr[offset],//当前正在处理的数据
+				lineid = offset;//用于ID赋值
+				if (self.opts.id) {lineid = Data[self.opts.id]}//如果有,则使用指定字段的值作为ID
+				$("#field",self).after("<tr class=\"grid-content\" id=\"line"+lineid+"\"></tr>");
+				thisline = $("#line"+lineid,self);
+					for ( offset in self.opts.field) {
+						Field = self.opts.field[offset];//当前字段对象
 						//检查是否有需要替换的节点内容
-						if (this.opts.replace) {
-							this.opts.dataArr[offset][key]=this.opts.replace(this.opts.dataArr[offset][key]);
+						if (typeof(Field.replace)=="function") {
+							Data[Field.field]=Field.replace(Data[Field.field]);
 						};
-						thisline.append("<td id=\""+this.opts.field[key]+tempID+"\">"+this.opts.dataArr[offset][key]+"</td>");
+						thisline.append("<td class=\""+Field.field+"\" id=\""+Field.field+lineid+"\">"+Data[Field.field]+"</td>");
 					}
 			}
-
-	};
+	}
+	self.beautyfy = function(){
+		self.find("tr:odd").addClass("oddRow");
+	}
+	function FieldOnClick (Grid){
+		console.log("self.onClick")
+		console.log(self)
+	}
 	//清空当前元素以承载Grid及其数据
-	this.empty();
-	this.append("<tr id=\"field\"></tr>")
-	this.popularfield();
-	this.popularcontent();
-	this.append("<div id=\"pager\" style=\"text-align: center;\">");
-	this.pager(this.opts.param.page, this.opts.totalpage);
+	self.empty();
+	self.append("<table><tr id=\"field\"></tr></table>");
+	self.popularfield();
+	self.popularcontent();
+	self.append();
+	self.constructpager(self.opts.param.page, self.opts.totalpage);
+	self.beautyfy();
 	//为未来元素绑定事件和响应动作
-	$("a.pager",this).click(function(){grid.changePage(this.id)});
-	if (typeof(this.opts.onClick)=="function") {$("td",this).click(this.opts.onClick);};
-	return this;
-
+	$("a.pager",self).click(function(){self.changePage(this.id)});
+	$("tr",     self).click(function(){console.log(this.id+"onTrClick")})
+	$("td",$("#field",self)).click(function(){console.log(self)});
+	if (typeof(self.opts.onClickTd)=="function") {$("td",self).click(self.opts.onClickTd);};
+	return self;
 }
 $.fn.Grid.defaults = {
 	field:{key:"notice"},
-	id:"id",
+	id:false,
 	dataArr:[{key:"your have passed nothing,data or url"}],//use a json object or not
 	url:false,//use ajax to get data from server or not
-	param:{"page":1,"pagesize":5},//if send param with ajax
+	param:{"page":1, "pagesize":5},//if send param with ajax
+	former5pages:false,
 	replace:false,//function,if replace some data or format it
+	events:false,
 	onClick:false,//call back
+	onMouseOverTr:false,
+	onClickTr:false,
+	onMouseOverTd:false,
+	onClickTd:false,
+	callback:false,
 	onMouse:false//call back
 }
 $.fn.Grid.setDefaults = function (userDefault){
 	return $.fn.Grid.defaults=$.extend(true,{},$.fn.Grid.defaults,userDefault);
 }
 })(jQuery);
-
-var uniqueID = (function() {
-	    var id = 0;
-	    return function() { return id++; };
-	})();
-
 (function($){  
-        $.fn.serializeJson=function(){//I copied this from wherever only god know it
-            var serializeObj={};  
-            $(this.serializeArray()).each(function(){  
-                serializeObj[this.name]=this.value;  
-            });  
-            return serializeObj;  
-        };  
-    })(jQuery);  
+    $.fn.serializeJson=function(){//I copied this from wherever only god know it
+        var serializeObj={};  
+        $(this.serializeArray()).each(function(){  
+            serializeObj[this.name]=this.value;  
+        });  
+        return serializeObj;  
+    };  
+})(jQuery);  
